6abeef2beb6467c833d2ea12ea7f9923cbbc6ec3
Androoon <andrejsuslov308@gmail.com>
2024-02-10 00:08:31 +0300

    feat: added config for hiber and session factory

---
+++ src/main/java/org/example/util/HibernateSessionFactory.java
@@ -0,0 +1,33 @@
+package org.example.util;
+
+import org.example.model.Course;
+import org.example.model.CourseResult;
+import org.example.model.Role;
+import org.example.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    public HibernateSessionFactory() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(Role.class);
+                configuration.addAnnotatedClass(Course.class);
+                configuration.addAnnotatedClass(CourseResult.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return sessionFactory;
+    }
+}
