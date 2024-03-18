df34a9cd9d958c7daa8c2c9c4264df45f264f7c1
Androoon <andrejsuslov308@gmail.com>
2024-03-18 22:43:32 +0300

    feat: added authority repo

---
+++ src/main/java/by/andron/repository/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package by.andron.repository;
+
+import by.andron.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+
+}
